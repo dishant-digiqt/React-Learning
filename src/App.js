bdd994b1541bb9679fd376c58f844da99b114699
dishant-digiqt <dishant.rathi@digiqt.com>
2021-09-15 17:28:25 +0530

    Count ++

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from "react";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
-import productsData from "./vschoolProducts";
-import Product from "./product";
+import React, { Component } from "react";
+// import Header from "./components/Header";
+// import MainContent from "./components/MainContent";
+// import Footer from "./components/Footer";
+// import productsData from "./vschoolProducts";
+// import Product from "./product";
 
-function App() {
-  const productComponents = productsData.map((item) => (
-    <Product Key={item.id} product={item} />
-  ));
-  return <div>{productComponents}</div>;
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      }
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClick}>Change!</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+
+export default App
