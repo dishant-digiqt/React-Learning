4d8630817a7a21e383c98d5ab34e971296451a24
dishant-digiqt <dishant.rathi@digiqt.com>
2021-09-15 14:21:53 +0530

    parent-child functions

---
+++ src/components/myInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo() {
+    return (
+      <div>
+        <h1>Dishant Rathi</h1>
+        <p>This is the Paragraph about me.....</p>
+        <ul>
+          <li>Thailand</li>
+          <li>Japan</li>
+          <li>NYC</li>
+        </ul>
+      </div>
+    );
+  }
+
+  export default MyInfo
--- src/index.js
+++ src/index.js
@@ -3,27 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+import MyInfo from "./components/myInfo.js"
 // ReactDOM.render(
 //   <React.StrictMode>
 //     <App />
 //   </React.StrictMode>,
 //   document.getElementById('root')
 // );
-function MyInfo() {
-  return (
-    <div>
-      <h1>Dishant Rathi</h1>
-      <p>This is the Paragraph about me.....</p>
-      <ul>
-        <li>Thailand</li>
-        <li>Japan</li>
-        <li>NYC</li>
-      </ul>
-    </div>
-  );
-}
 
-ReactDOM.render(<MyInfo />, document.getElementById("root"));
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 reportWebVitals();
