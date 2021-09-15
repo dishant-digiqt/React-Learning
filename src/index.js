42486e29a4199e41377794359e3a89c54859c071
dishant-digiqt <dishant.rathi@digiqt.com>
2021-09-15 13:29:47 +0530

    IC

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+function MyInfo() {
+  return (
+    <div>
+      <h1>Dishant Rathi</h1>
+      <p>This is the Paragraph about me.....</p>
+      <ul>
+        <li>Thailand</li>
+        <li>Japan</li>
+        <li>NYC</li>
+      </ul>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(<MyInfo />, document.getElementById("root"));
+
 reportWebVitals();
